9f6dc98148cd175f2d1c24d6393fabd918eb89f3
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-05-12 22:38:56 +0545

    Add search filter in country

--- apps/country/gh_filters.py
+++ apps/country/gh_filters.py
@@ -1,5 +1,6 @@
 import strawberry
 from strawberry.django import auto
+from django.db.models import Q
 from .models import (
     Country,
     CountryAdditionalInfo,
@@ -15,6 +16,16 @@ class CountryFilter:
     iso3: auto
     name: auto
     idmc_names: auto
+    search: str | None
+
+    def filter_search(self, queryset):
+        if not self.search:
+            return queryset
+        return queryset.filter(
+            Q(name__icontains=self.search) |
+            Q(idmc_names__icontains=self.search) |
+            Q(iso3__icontains=self.search)
+        )
 
 
 @strawberry.django.filters.filter(CountryAdditionalInfo, lookups=True)
