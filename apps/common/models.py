baa56206f0ef896908ef5f40cd918c2fcb8b1710
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-05-27 09:52:54 +0545

    Fix typo

--- apps/common/models.py
+++ apps/common/models.py
@@ -6,7 +6,7 @@ class StaticPage(models.Model):
     class StaticPageType(models.TextChoices):
         GOOD_PRACTICE_LISTING_PAGE = 'good_practice_listing_page', _('Good practice listing page')
         SUBMIT_GOOD_PRACTICE = 'submit_good_practice', _('Submit good practice')
-        GOOD_PRACTICE_CONTACT_INFORMATION = 'good_practice_cotact_information', _('Good practice cotact information')
+        GOOD_PRACTICE_CONTACT_INFORMATION = 'good_practice_contact_information', _('Good practice contact information')
 
     type = models.CharField(
         max_length=255, verbose_name=_('Static page type'), choices=StaticPageType.choices
