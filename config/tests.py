184945f36b231b906c816e49cdf5028e694e70eb
thenav56 <ayernavin@gmail.com>
2022-11-08 16:49:02 +0545

    Run test in CI.

--- config/tests.py
+++ config/tests.py
@@ -49,3 +49,12 @@ class TestCase(BaseTestCase):
         """
         content = resp.json()
         self.assertIn("errors", list(content.keys()), msg or content)
+
+
+class FakeTest(TestCase):
+    """
+    This test is for running migrations only
+    docker-compose run --rm server ./manage.py test -v 2 --pattern="config/tests/test_fake.py"
+    """
+    def test_fake(self):
+        pass
