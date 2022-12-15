2a780fdc1b3a2df991d3e9b8b8a0f787472a3c72
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-12-15 12:29:18 +0545

    Improve good practice mutation

--- apps/good_practice/mutations.py
+++ apps/good_practice/mutations.py
@@ -1,9 +1,5 @@
-import datetime
 import strawberry
-from django.db import transaction
-# from django.core.exceptions import ValidationError
-from rest_framework.exceptions import ValidationError
-from typing import List, Optional, Union
+from typing import List, Optional, TypeVar, Generic
 from strawberry.file_uploads import Upload
 from strawberry_django_plus import gql
 
@@ -11,49 +7,66 @@ from .enums import (
     TypeEnum,
     StageTypeEnum,
 )
-from config.utils import validate_hcaptcha
 from apps.good_practice.models import GoodPractice
 from apps.good_practice.types import GoodPracticeType
 
-from config.utils import get_values_list_from_dataclass
-from strawberry_django_plus.types import OperationInfo
-
 from .serializers import GoodPracticeSerializer
-from strawberry_utils.error_types import _CustomErrorType, mutation_is_not_valid
+from strawberry_utils.error_types import CustomErrorType, mutation_is_not_valid
+from asgiref.sync import sync_to_async
+
+
+MutationVar = TypeVar('MutationVar')
 
 
 @strawberry.type
-class BaseInputType:
+class MutationResponse(Generic[MutationVar]):
     ok: bool
-    errors: Optional[List[_CustomErrorType]]
+    errors: Optional[CustomErrorType]
+    data: Optional[MutationVar]
 
 
-class GoodPracticePageViewCountType(BaseInputType):
-    id: gql.auto
-    page_viewed_count: gql.auto
+@strawberry.type
+class Response:
+    ok: bool
+    errors: Optional[CustomErrorType]
+    data: Optional[GoodPracticeType]
+
+
+@strawberry.type
+class GoodPracticePageViewCountType:
+    id: strawberry.ID
+    page_viewed_count: int
 
 
 @strawberry.input
 class GoodPracticeInputType:
+    # TODO: Use serializer to generate this type
+    start_year: int
+    end_year: int
+
+    # Enum fields
     type: TypeEnum
     stage: StageTypeEnum
-    start_year: int
-    countries: List[str]
-    drivers_of_displacement: List[str]
-    focus_area: List[str]
-    tags: List[str]
-    end_year: datetime.date
-    implementing_entity: str
-    implementing_entity_fr: Optional[str]
-    # image: gql.auto
-    description: Optional[str]
-    description_fr: Optional[str]
-    published_date: datetime.date
-    media_and_resource_links: Optional[str]
-    media_and_resource_links_fr: Optional[str]
-    title: str
-    title_fr: Optional[str]
-    # captcha: gql.auto
+
+    # M2M fields
+    countries: List[strawberry.ID]
+    drivers_of_displacement: List[strawberry.ID]
+    focus_area: List[strawberry.ID]
+    tags: List[strawberry.ID]
+
+    image: Upload
+
+    # English fields
+    title_en: Optional[str] = None
+    description_en: Optional[str] = None
+    media_and_resource_links_en: Optional[str] = None
+    implementing_entity_en: Optional[str] = None
+
+    # French fields
+    title_fr: Optional[str] = None
+    description_fr: Optional[str] = None
+    media_and_resource_links_fr: Optional[str] = None
+    implementing_entity_fr: Optional[str] = None
 
 
 @strawberry.type
@@ -66,71 +79,15 @@ class Mutation:
         return obj
 
     @strawberry.mutation
+    @sync_to_async
     def create_good_practice(
         self,
         info,
         input: GoodPracticeInputType,
-    ) -> Union[GoodPracticeType, BaseInputType]:
+    ) -> MutationResponse[GoodPracticeType]:
         data = vars(input)
         serializer = GoodPracticeSerializer(data=data)
         if errors := mutation_is_not_valid(serializer):
-            return BaseInputType(errors=errors, ok=False)
+            return MutationResponse(errors=errors, ok=False, data=None)
         instance = serializer.save()
-        return instance
-        # pass
-
-
-
-
-    # @gql.django.input_mutation
-    # def create_good_practice(
-    #     self,
-    #     info,
-    #     type: TypeEnum,
-    #     stage: StageTypeEnum,
-    #     start_year: int,
-    #     countries: List[int],
-    #     drivers_of_displacement: List[int],
-    #     focus_area: List[int],
-    #     tags: List[int],
-    #     end_year: Optional[int] = None,
-    #     implementing_entity: Optional[str] = None,
-    #     implementing_entity_fr: Optional[str] = None,
-    #     image: Optional[Upload] = None,
-    #     description: Optional[str] = None,
-    #     description_fr: Optional[str] = None,
-    #     published_date: Optional[datetime.datetime] = None,
-    #     media_and_resource_links: Optional[str] = None,
-    #     media_and_resource_links_fr: Optional[str] = None,
-    #     title: Optional[str] = None,
-    #     title_fr: Optional[str] = None,
-    #     captcha: Optional[str] = None,
-    # ) -> GoodPracticeType:
-    #     if not validate_hcaptcha(captcha):
-    #         raise ValidationError('Captcha is invalid. Please try again')
-    #     with transaction.atomic():
-    #         obj = GoodPractice.objects.create(
-    #             title=title,
-    #             title_fr=title_fr,
-    #             description=description,
-    #             description_fr=description_fr,
-    #             type=type,
-    #             stage=stage,
-    #             start_year=start_year,
-    #             end_year=end_year,
-    #             implementing_entity=implementing_entity,
-    #             implementing_entity_fr=implementing_entity_fr,
-    #             image=image,
-    #             published_date=published_date,
-    #             media_and_resource_links=media_and_resource_links,
-    #             media_and_resource_links_fr=media_and_resource_links_fr,
-    #             is_public=True,
-    #         )
-    #         obj.save()
-
-    #         #  save M2m
-    #         obj.countries.add(*countries)
-    #         obj.tags.add(*tags)
-    #         obj.drivers_of_displacement.add(*drivers_of_displacement)
-    #         obj.focus_area.add(*focus_area)
-    #         return obj
+        return MutationResponse(errors=None, ok=True, data=instance)
